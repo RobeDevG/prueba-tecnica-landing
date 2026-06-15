8d5617e0c912602dacc37170843a8bcc4a5cd8e3
RobeDevG <robertodev97@gmail.com>
2026-06-15 17:30:53 -0400

    [ADD] add next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   output: "export",
+  reactStrictMode: false,
+  distDir: "dist",
+  trailingSlash: true,
 };
 
 export default nextConfig;
